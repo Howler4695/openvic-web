a10d31438e1236d6594880396c2a39c062e84519
hayden howle <howler4695@gmail.com>
2023-04-17 23:06:33 -0500

    created

--- src/frontend/pages/_app.ts
+++
@@ -1,7 +0,0 @@
-import "../styles/global.css";
-
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default App;
--- src/frontend/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Head from "next/head";
-import Image from "next/image";
-
-const Home = () => {};
-
-export default Home;
