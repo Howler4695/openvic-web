c1e3b2f4bf49464fb4912c60c9d4cb4993ff6c5a
Hayden Howle <hayden.howle@teambespin.us>
2023-06-01 10:44:55 -0500

    progress

--- components/HeroBody.tsx
+++ components/HeroBody.tsx
@@ -1,19 +1,35 @@
 import React, { FC, PropsWithChildren } from "react";
 import HeroHeader from "./HeroHeader";
 import Image from "next/image";
-import { BrowserRouter } from "react-router-dom";
 
 interface HeroBodyProps {
+  className?: string;
+  contentBackground?: string;
   children?: any;
 }
 
-const HeroBody: FC<HeroBodyProps> = ({}) => {
+const HeroBody: FC<PropsWithChildren<HeroBodyProps>> = ({
+  className,
+  contentBackground,
+  children,
+}) => {
   // const minimumHeight = calc(100vh - 70px - 2em)
 
   return (
-    <div className="flex flex-col bg-gray-100 w-full mx-4 rounded-[40px] my-12  md:mx-12 lg:mx-32 xl:mx-60 overflow-y-hidden">
+    <div
+      className={
+        className ??
+        "flex flex-col bg-gray-100 w-full mx-4 rounded-[40px] my-12  md:mx-12 lg:mx-32 xl:mx-60 overflow-y-hidden"
+      }
+    >
       <HeroHeader />
-      <div className=" flex h-full rounded-[40px] mx-2 mb-2  bg-gray-300 xl:mx-4 xl:mb-4"></div>
+      <div
+        className={`flex h-full rounded-[40px] mx-2 mb-2  ${
+          contentBackground ?? "bg-gray-300"
+        } xl:mx-4 xl:mb-4`}
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,20 @@ import HeroBody from "@/components/HeroBody";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const OPEN_VIC_DESC =
+  "OpenVic aims to faithfully recreate Victoria 2: Heart of Darkness with a focus on enhancing performance, stability, and modability. We will support loading Victoria 2 mods with zero modifications in a classic / compatibility mode, which will be availible if you own a legitimate copy of Victoria 2. OpenVic's default mode will be completely free, with our own graphics, content, data, and soundtrack, keeping the overall feel, playstyle, and balance as Victoria 2. OpenVic will have its own modding api, allowing for extremely detailed and deep mods, while being easier to mod and easier for users to find and install mods. If you are interested in helping, see the contribute page, and if you want to stay up to date between dev diaries, check out the GitHub and join the Discord, we can't wait to get to know you.";
+
 export default function Home() {
   return (
     <Page>
-      <HeroBody />
+      <HeroBody contentBackground="bg-[#471e1e]">
+        <div className="flex flex-col font-serif w-full items-center py-8 px-2">
+          <h1 className="text-7xl text-gray-200  font-bold px-16">Our Goal</h1>
+          <p className="text-4xl text-gray-200 text-center font-semibold px-16 py-8">
+            {OPEN_VIC_DESC}
+          </p>
+        </div>
+      </HeroBody>
     </Page>
   );
 }
